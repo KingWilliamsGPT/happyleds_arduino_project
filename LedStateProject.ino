d28e79705f66a59d281c0732223aca26ba5568b8
KingWilliamsGPT <williamusanga22@gmail.com>
2024-08-22 10:33:03 +0100

    fix

--- LedStateProject.ino
+++ LedStateProject.ino
@@ -2,7 +2,7 @@
 #define LED2 3
 #define LED3 4
 #define BTN1 5
-#define BOUNCE_DELAY 40 //miliseconds
+#define BOUNCE_DELAY 10 //miliseconds
 #define SERIAL_SPEED 9600
 
 
@@ -16,7 +16,9 @@ class Btn{
         }
 
         bool isPressed(){
-            if digitalRead(this->pin == HIGH){
+            // Serial. 
+            // The button says yes to is pressed after it has just been depressed
+            if (digitalRead(this->pin == HIGH)){
                 if (this->isTouched){
                     this->isTouched == false;
                     return true;
@@ -25,8 +27,9 @@ class Btn{
                 // beign touched coz pin == LOW
                 this->isTouched == true;
                 delay(BOUNCE_DELAY);
-                return ret;
             }
+
+            return false;
         }
 
 };
@@ -34,7 +37,8 @@ class Btn{
 
 class Project{
     private:
-        Btn _btn;
+        bool _is_pressed = false;
+        int state = 0;
 
     public: 
         int led1, led2, led3, btn;
@@ -45,9 +49,6 @@ class Project{
             this->led2 = led2;
             this->led3 = led3;
             this->btn = btn;
-
-            Btn _btn(btn);
-            this->_btn = _btn;
         }
 
         void setup(){
@@ -62,13 +63,33 @@ class Project{
 
             pinMode(btn, INPUT);
 
-            // Serial.println("--SETUP COMPLETE--");
+            Serial.println("--SETUP COMPLETE2--");
         }
 
         void loop(){
-            if (this->_btn.isPressed()){
-                Serial.println("Im depressed");
+            if(this->buttonIsClicked()){
+                // switch state 
             }
+        }
+
+        void setPressed(bool v){
+            this->_is_pressed = v;
+        }
+
+        bool buttonIsClicked(){
+            if (digitalRead(this->btn) == HIGH){
+                if(!this->_is_pressed){
+                    setPressed(true);
+                }    
+            } else{
+                if(this->_is_pressed){
+                    // Serial.println("clicked!!");
+                    setPressed(false);
+                    delay(BOUNCE_DELAY);
+                    return true;
+                }
+            }
+            return false;
         }
 };
 
